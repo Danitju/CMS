743b4e5c6ffc0e6dd56075af38c0f0d2a70e10c4
Dani <daniaragonessantamaria@gmail.com>
2018-11-21 11:29:30 +0100

    2e update

--- productpagina.php
+++ productpagina.php
@@ -29,9 +29,9 @@
             <ul class="list-unstyled components">
                 <p>Frouks</p>
                 <li>
-                    <a href="index.php" data-toggle="collapse" aria-expanded="false">Home</a>
-                
-                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li class="active">
                     <a href="productpagina.php">Producten toevoegen</a>
                 </li>
                 <li>
@@ -70,7 +70,7 @@
                 </div>
             </nav>
 
-            <h2>Home pagina</h2>
+            <h2>Product toevoegen</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
