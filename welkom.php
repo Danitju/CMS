7d31c372bd51453935cd19e6997dc76f99942704
Dani <daniaragonessantamaria@gmail.com>
2019-01-15 23:06:16 +0100

    Bestanden uploaden

--- welkom.php
+++ welkom.php
@@ -1,16 +1,29 @@
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>badmemes</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="cms.css" />
-    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-
-</head>
-<body>
-<h1 class="title1">Frouks</h1>
-<div class="upload">
-</div>
+<!DOCTYPE html><!--HEADER-->
+<html><!--HOMEPAGE-->
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="styleGrid.css">
+		<link rel="stylesheet" href="responsive.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<header class="header-main">
+			<!--<input class="search" type="text">-->
+			<a href="index.html"><img class="logo" src="frouks_logo.svg"></a><!--Link naar de homepage-->
+			<nav class="top-nav">
+				<div class="menu-button"><i class="fas fa-bars"></i></div>
+				<ul>
+					<li class="nav-item"><a href="#">Collectie <i class="fas fa-caret-down dropdown"></i></a></li>
+					<li class="nav-item"><a href="#">Workshops</a></li>
+					<li class="nav-item"><a href="stenen_overzicht.html">Stenen Overzicht</a></li>
+					<li class="nav-item"><a href="#">Over Frouks</a></li>
+					<li class="nav-item"><a href="#">Evenementen</a></li>
+					<li class="nav-item"><a href="#">Locatie</a></li>
+					<li class="nav-item"><a href="#">Contact</a></li>
+				</ul>
+			</nav>
+		</header><!--END HEADER-->
 <?php 
 $mysqli = new mysqli('localhost', 'root', 'root', '24994_db') or die ('Error connecting');
 $query = "SELECT location, title, description FROM images ORDER BY image_id DESC";
@@ -18,20 +31,20 @@ $stmt = $mysqli->prepare($query) or die ('Error preparing');
 $stmt->bind_result($location, $title,$description) or die ('Error binding results');
 $stmt->execute() or die ('Error executing');
 
-echo '<div class="main">';
+echo '<main>';
 echo '<section>';
+echo '<div class="grid-container grid-columns-three">';
 while ($succes = $stmt->fetch()){
-    
-    echo '<div class="grid-container grid-columns-three">';
-    echo '<img class="thumbnail" src="' . $location . '" />';
-    // echo ' ' . $title . '<br>';
-    // echo ' ' . $description . '<br>'; 
-    echo '</div>';
-
-  
-
+    echo '<img class="thumbnail" src=" ' . $location . '">';
 }
-echo '</section>';
 echo '</div>';
+echo '</section>';
+echo '</main>';
+
+				
+				
+		
 ?>
-</body>
+<script src="scripts/script.js"></script>
+	</body>
+</html>
